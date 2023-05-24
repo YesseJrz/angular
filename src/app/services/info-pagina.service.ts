814f041f3096c3c7fea122ada5913f2a231342d5
Marian <maryjrz25@gmail.com>
2023-05-23 19:56:21 -0600

    terminado

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { InfopPagina } from '../interfaces/info-pagina.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfopPagina = {};
+  cargada = false;
+
+  equipo: any[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.CargarEquipo();
+  }
+
+  private cargarInfo(){
+    //Leer el archivo JSON
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe( (resp: InfopPagina) => {
+      this.cargada = true;
+      this.info = resp;
+
+      });
+  }
+
+  private CargarEquipo(){
+    //Leer el archivo JSON
+    this.http.get('https://protafolio2-default-rtdb.firebaseio.com/equipo.json')
+    .subscribe((resp: any) => {
+      this.equipo = resp;
+      console.log(resp);
+
+   } );
+  }
+}
